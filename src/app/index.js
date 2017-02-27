702792cb307f4280548e3c189a756972a6963b92
Shashank Jaiswal <shashank.jaiswal@cuelogic.com>
2017-02-27 13:03:40 +0530

    Creating components in react

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
 
-class App extends React.Component{
-    render(){
-        return(
-            <div className="container">
-                <h1>Hello world!</h1>
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+               <Header/>
+               <Home/>
             </div>
         );
     }
 }
 
-render(<App/>,document.getElementById('app'));
+render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-    entry: SRC_DIR + "/app/index.jsx",
+    entry: SRC_DIR + "/app/index.js",
     output: {
         path: DIST_DIR + "/app",
         filename: "bundle.js",
@@ -13,7 +13,7 @@ var config = {
     },
     module:{
         loaders: [{
-            test: /\.jsx?/,
+            test: /\.js?/,
             include: SRC_DIR,
             loader:"babel-loader",
             query:{
