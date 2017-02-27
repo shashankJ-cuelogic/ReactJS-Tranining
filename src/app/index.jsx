68d212133ab73b7baaea6d40b138bbeab03f3aa0
Shashank Jaiswal <shashank.jaiswal@cuelogic.com>
2017-02-27 12:25:20 +0530

    Init component

---
+++ src/app/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className="container">
+                <h1>Hello world!</h1>
+            </div>
+        );
+    }
+}
+
+render(<App/>,document.getElementById('app'));
