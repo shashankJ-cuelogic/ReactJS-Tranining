19be6a91e9fb8d9bf01e5656e93bec50fa620563
Shashank Jaiswal <shashank.jaiswal@cuelogic.com>
2017-03-01 15:42:49 +0530

    Routing in ReactJS

--- src/app/components/Root.js
+++ src/app/components/Root.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 
-export class Home extends React.Component {
-    render(){
-        return(
+export class Root extends React.Component {
+    render() {
+        return (
             <div>
-                <h1>Home</h1>
+                {this.props.children}
             </div>
         );
     }
--- src/app/index.js
+++ src/app/index.js
@@ -1,47 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
+import {Root} from "./components/Root";
+import {Home} from "./components/Home";
+import {About} from "./components/About";
+import {Contact} from "./components/Contact";
+import { Route, Router, browserHistory } from "react-router"
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state={
-            header:"Showing user information"            
-        }
-    }
-
-    onGreet(){
-        alert("Hello tester!");
-    }
-
-    onChangeHeader(value){
-        this.setState({
-            header:value
-        });
-    }
-
     render() {
-        var users={
-            name:"Shashank",
-            age:27,
-            occupation:"Private Job",
-            designation:"SSE",
-            hobbies:["Movies","Sports"]
-        };
-
-        
-
         return (
-            <div className="container theme-showcase">
-               <Header text={this.state.header}/>
-               <Home users={users} 
-               greet={this.onGreet}
-               onheaderChange={this.onChangeHeader.bind(this)}
-               />
-               <Home greet={this.onGreet} onheaderChange={this.onChangeHeader.bind(this)}/>
-            </div>
+            <Router history={browserHistory}>
+                <Route path={"/"} component={Root}>
+                    <Route path={"home"} component={Home} />
+                    <Route path={"about"} component={About} />
+                    <Route path={"contact"} component={Contact} />
+                </Route>
+            </Router>
         );
     }
 }
